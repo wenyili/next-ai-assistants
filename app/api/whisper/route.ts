3f2ae02c316fff9fe90f07adf28bdd6f1eae4289
liwenyi <wenyi.li@cocos.com>
2024-02-27 10:43:10 +0800

    debug: whisper response file format error on ios

--- app/api/whisper/route.ts
+++ app/api/whisper/route.ts
@@ -1,10 +1,8 @@
 // Import necessary libraries
 import OpenAI, { toFile } from 'openai';
-import fs from 'fs';
 import { NextResponse } from "next/server";
 
-// Promisify the exec function from child_process
-const util = require('util');
+// export const runtime = 'edge'
 
 const apiKey = process.env.AZURE_OPENAI_API_KEY_WHISPER
 const resource = process.env.AZURE_OPENAI_RESOURCE_WHISPER
@@ -19,23 +17,25 @@ const openai = new OpenAI({
 })
 
 export async function POST(req: Request) {
-  const buffer = await req.blob()
-  try {
-    // Convert the audio data to text
-    const translation = await openai.audio.transcriptions.create({
-        file: await toFile(buffer, 'speech.mp3'),
-        model: 'whisper-1',
-      });
-    // Return the transcribed text in the response
-    return NextResponse.json({result: translation.text}, {status:200});
-  } catch(error: any) {
-    // Handle any errors that occur during the request
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      return NextResponse.json({ error: error.response.data }, {status:500});
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      return NextResponse.json({ error: "An error occurred during your request." }, {status:500});
+    const buffer = await req.blob()
+    try {
+        const file = await toFile(buffer, 'speech.mp3', {type:　"mp3"})
+        console.log(`file: name->${file.name} type->${file.type} size->${file.size}`)
+        // Convert the audio data to text
+        const translation = await openai.audio.transcriptions.create({
+            file: file,
+            model: 'whisper-1',
+        });
+        // Return the transcribed text in the response
+        return NextResponse.json({result: translation.text}, {status:200});
+    } catch(error: any) {
+        // Handle any errors that occur during the request
+        if (error.response) {
+        console.error(error.response.status, error.response.data);
+        return NextResponse.json({ error: error.response.data }, {status:500});
+        } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        return NextResponse.json({ error: "An error occurred during your request." }, {status:500});
+        }
     }
-  }
 }
