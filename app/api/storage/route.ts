9d126fb7d15610fcbc38281fb33d5158cbd386d7
liwenyi <wenyi.li@cocos.com>
2024-04-24 11:36:21 +0800

    feat: using env for storage config

--- app/api/storage/route.ts
+++ app/api/storage/route.ts
@@ -3,10 +3,10 @@ import OSS from 'ali-oss';
 
 // 初始化OSS客户端。请将以下参数替换为您自己的配置信息。
 const client = new OSS({
-  region: 'oss-cn-hangzhou', // 示例：'oss-cn-hangzhou'，填写Bucket所在地域。
+  region: process.env.OSS_REGION, // 示例：'oss-cn-hangzhou'，填写Bucket所在地域。
   accessKeyId: process.env.OSS_ACCESS_KEY_ID!, // 确保已设置环境变量OSS_ACCESS_KEY_ID。
   accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET!, // 确保已设置环境变量OSS_ACCESS_KEY_SECRET。
-  bucket: 'chats-storage', // 示例：'my-bucket-name'，填写存储空间名称。
+  bucket: process.env.OSS_BUCKET, // 示例：'my-bucket-name'，填写存储空间名称。
 });
 
 export async function GET() {
